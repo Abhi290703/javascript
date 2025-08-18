5875cecb95b64f65856946860b0bfe77d09c2b25
Abhi290703 <abhigec6@gmail.com>
2025-08-18 20:08:03 +0530

    commit all

---
+++ operators/logical.js
@@ -0,0 +1,19 @@
+console.log(true && false || true); 
+console.log(false || true && true);
+console.log(!true && false);
+console.log(!false);    
+console.log(!(""));
+console.log(!(" "))
+console.log(("hello"));  
+console.log(!("hello" && "world"));
+console.log(true || false && true && false || true);
+console.log(!undefined);
+console.log(!null);
+console.log(!(undefined && null));
+console.log(!("hello" && "world")); 
+console.log(!("hello" || "world"));
+console.log(!("hello" && "false"));
+console.log(!("false" || "true"));
+console.log(!("false" && "true"));
+console.log(!("false" && 5));
+console.log(!("true" && 5));
---
+++ operators/unary operator.js
@@ -0,0 +1,34 @@
+// increment operator
+var s = 10;
+console.log(s);
+console.log(s++);
+console.log(++s);
+
+// decrement operator
+var s1 = 11;
+console.log(s1);
+console.log(s1--);
+console.log(--s1);
+
+// sample program
+let n = -5;
+            console.log(n);
+            let n1 = -"5";
+            console.log(n1);
+            let n2 = -"Hello";
+            console.log(n2);
+
+let m = +5;
+console.log(m);
+let m1 = +5;
+console.log(m1);
+let m2 = +"Hello";
+console.log(m2);
+
+// unary operator
+var a = 10;
+console.log(a++);
+console.log(++a);
+console.log(a--);
+console.log(--a);
+console.log(a);
