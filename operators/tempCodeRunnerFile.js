866a1d07978829df1a9cdb5dfd2b50c8399a2251
Abhi290703 <abhigec6@gmail.com>
2025-08-25 09:31:10 +0530

    commit all

--- operators/tempCodeRunnerFile.js
+++ operators/tempCodeRunnerFile.js
@@ -1,5 +1,13 @@
-et j = 1;
-    while (j<=i){
-        pattern += "* ";
-        j++;
-    }
+i = 1;
+// let row = 5;
+// while (i <= row) {
+//     let pattern = "";
+//     let j = 1;
+//     while (j<=i){
+//         pattern += "* ";
+//         j++;
+//     }
+//     i++;
+//     console.log(pattern);
+// }
+// console.log("***************");
--- operators/while.js
+++ operators/while.js
@@ -16,19 +16,19 @@
 //     c++;
 // }
 
-let i = 1;
-let row = 5;
-while (i <= row) {
-    let pattern = "";
-    let j = 1;
-    while (j<=i){
-        pattern += "* ";
-        j++;
-    }
-    i++;
-    console.log(pattern);
-}
-console.log("***************");
+// let i = 1;
+// let row = 5;
+// while (i <= row) {
+//     let pattern = "";
+//     let j = 1;
+//     while (j<=i){
+//         pattern += "* ";
+//         j++;
+//     }
+//     i++;
+//     console.log(pattern);
+// }
+// console.log("***************");
 
 let j = 5;
 let row1 = 1;
