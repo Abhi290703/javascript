6ad45d83829f7a8a38e6d55480c51158299a1793
Abhi290703 <abhigec6@gmail.com>
2025-08-25 11:08:37 +0530

    add

--- operators/while.js
+++ operators/while.js
@@ -68,35 +68,116 @@
 // let months = ["2013","2014","2015","2016","2017","2018","2019","2020","2021","2022","2023"]
 // console.log(months.length);
 
-// example 2
-let car = document.getElementById("car");
-let company = document.getElementById("company");
-let cost = document.getElementById("cost");
+ // example 2
+// let car = document.getElementById("car");
+// let company = document.getElementById("company");
+// let cost = document.getElementById("cost");
 
-let cars = ["ZEN", "Ritz" ,"Baleno", "Swift", "i10", "maruti800", "Altroz"]
-let companies = ["Suzuki", "Honda","Hyundai", "Tata",]
-let costs = [500000, 600000, 700000, 800000, 900000, 1000000, 1100000]
-console.log(cars.length);
-console.log(companies.length);
-console.log(costs.length);
+// let cars = ["ZEN", "Ritz" ,"Baleno", "Swift", "i10", "maruti800", "Altroz"]
+// let companies = ["Suzuki", "Honda","Hyundai", "Tata",]
+// let costs = [500000, 600000, 700000, 800000, 900000, 1000000, 1100000]
+// console.log(cars.length);
+// console.log(companies.length);
+// console.log(costs.length);
 
-for (let i = 0; i < cars.length; i++) {
-    car.innerHTML += `<option>${cars[i]}</option>`;
+// for (let i = 0; i < cars.length; i++) {
+//     car.innerHTML += `<option>${cars[i]}</option>`;
+// }
+
+// for (let i =0; i<companies.length; i++){
+//     companies.innerHTML += `<option>${companies[i]}</option>`;
+// }
+
+// for (let i =0; i<costs.length; i++){
+//     cost.innerHTML += `<option>${costs[i]}</option>`;
+// }
+
+// let stockitems = 10
+// let purchased = 5
+// while(stockitems <= purchased){
+//     stockitems -= purchased;
+//     console.log(`Items left in stock are: ${stockitems}`);
+//     console.log(`Items purchased are: ${purchased}`);
+// }
+// console.log(`available items ${stockitems} and purchased items ${purchased} try to remove ${stockitems - purchased}`)\
+
+let a = 0;
+while (a<=10){
+    console.log(a);
+    a++;
 }
 
-for (let i =0; i<companies.length; i++){
-    companies.innerHTML += `<option>${companies[i]}</option>`;
-}
 
-for (let i =0; i<costs.length; i++){
-    cost.innerHTML += `<option>${costs[i]}</option>`;
-}
+// let count = 1;
+// let pin = 1234;
 
-let stockitems = 10
-let purchased = 5
-while(stockitems <= purchased){
-    stockitems -= purchased;
-    console.log(`Items left in stock are: ${stockitems}`);
-    console.log(`Items purchased are: ${purchased}`);
+// do{
+//     console.log("Welcome to ICICI bank");
+//     if(pin == 1234){
+//         console.log("transaction succes");
+//     }
+//     else if(count == 3)
+//     else{
+//         console.log("ENter valid pin");
+//     }
+
+//     count++;
+
+// }while(count<=3);
+
+// if(count == 3){
+//     console.log("last attempt");
+// }
+// else if(count>3){
+// console.log("your card is blocked");
+// }
+
+
+// transcation
+
+// let count = 3;
+// let pin = 1234;
+
+// do{
+//     console.log("Welcome to ICICI bank");
+//     if(pin == 1234){
+//         console.log("transaction succes");
+//     }
+//     else if(count == 3){}
+//     else{
+//         console.log("ENter valid pin");
+//     }
+//     count++;
+
+
+// }while(count<=3);
+
+// if(count == 3){
+//     console.log("last attempt");
+// }
+// else if(count>3){
+// console.log("your card is blocked");
+// }
+
+
+let count = 4;
+let pin = 2346;
+do{
+    console.log("Welcome to ICICI bank");
+    if (pin == 2345){
+        console.log("transcation success")
+    }
+    else if(count == 3){}
+    else{
+        console.log("Enter valid pin");
+    }
+    count ++
 }
-console.log(`available items ${stockitems} and purchased items ${purchased} try to remove ${stockitems - purchased}`)
+while(count<=3){
+    if(count == 3){
+        console.log("over limited")
+    }
+    else{
+        console.log("your card is blocked");
+    }
+}
