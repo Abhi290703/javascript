4114b4c72352f8eafd77cc8b316227b489016dab
Abhi290703 <abhigec6@gmail.com>
2025-08-29 11:54:33 +0530

    add

---
+++ loops/operator.js
@@ -0,0 +1,59 @@
+/* 
+
+Operator : operator is a symbol that is used to perform operation or operand. it is used for binary and unary.
+
+Types of operations :
+Assignment operator
+Arthimatic operator
+Bitwise 
+conditional
+
+
+
+
+
+
+
+
+*/
+
+// Arthimatic operator
+
+// let a = 10;
+// let b = "20";
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+
+// Assignment operator
+
+let c = 20;
+let d = "hello";
+let e = 10;
+console.log(c += d);
+console.log(c += e);
+// console.log(a -= b);
+
+// comparsion operator
+
+// ==, ===, !=, !==, >, <, >=, <=,
+
+let num = 10;
+let num1 = "10";
+// console.log(num==num1);
+// console.log(num===num1);
+// console.log(num<num1);
+// console.log(num>num);
+// console.log(num<=num1);
+// console.log(num>=num);
+// console.log(num!=num1);
+// console.log(num!==num1);
+console.log(null == undefined)
+console.log(null != undefined)
+console.log(null !== undefined)
+console.log(null === "undefined")
+console.log(null <= "undefined")
+
+
