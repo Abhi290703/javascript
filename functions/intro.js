12fd8ac75fd86e110b0976494ba5db23fdb8178a
Abhi290703 <abhigec6@gmail.com>
2025-09-02 18:46:01 +0530

    add

---
+++ functions/intro.js
@@ -0,0 +1,102 @@
+// function  -> keyword
+// function data -> data is function name
+//function data(a) -> a is parameter
+
+// syntax 
+// function name(parameter){
+//     block of code
+// }
+// name (argunments)    // call function where it represent at the end of the block code
+
+// function data(a="hello")
+// {
+//     console.log(`this is javascript ${a}`)
+// } 
+// data(   )
+
+// return function
+
+// function data(a="hi"){
+
+//     return `this is ${a}`;
+// }
+
+// console.log (data());
+
+
+
+// console.log("Hello World");
+
+// Function:A function is a reusable block of code to perform a specified task
+// Types of Functions
+// 1.Function Declaration
+function greet(name) {
+    return ("Hello,How are you? " + name); //create a element
+ }
+console.log(greet("Abilash"))
+
+
+// with return value
+
+function ask(permission){
+    return("class" + permission)
+}
+console.log(ask("come in"))
+
+// first checks call function and go to function parameter and than checks the statement and produces o/p
+
+// without return value
+
+function greet(name) {
+    console.log("Hi,Hearty Congratulations" , name)
+
+}
+greet("Ravi")
+
+function greet(name){
+     a =  "Hi,Heart congrualations " +  name
+    console.log(a)
+}
+greet("Ramu")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
