3d4e757ae1c05a69b99c9f4e6b55e93b2c7dd763
Abhi290703 <abhigec6@gmail.com>
2025-09-03 00:05:25 +0530

    commit

--- functions/assignment1.js
+++ functions/assignment1.js
@@ -1,1 +1,18 @@
-//  
+// fizzbuzz
+
+function fizzbuzz(num){
+    let arr =[]
+    if (num % 3 == 0 && num % 5 == 0){
+        return fizzbuzz
+    }
+    else if (num % 3 == 0){
+        return fizz
+    }
+    else if (num % 5 == 0){
+        return buzz
+    }
+    else {
+        return (none)
+    }
+}
+console.log(fizzbuzz(15))
