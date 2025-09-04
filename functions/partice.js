40cc2aaeaf98098fc26baeb899aaf3c4807246c4
Abhi290703 <abhigec6@gmail.com>
2025-09-04 15:08:22 +0530

    add

--- functions/partice.js
+++ functions/partice.js
@@ -1,21 +1,35 @@
-function greet (name="dear") {
-    a  = "Good Morning " + name
-    console.log(a)
+// function greet (name="dear") {
+//     a  = "Good Morning " + name
+//     console.log(a)
+// }
+// greet()
+
+
+
+// // increment and decrement
+
+// let value = 0;
+
+//     function increment() {
+//       value++;
+//       document.getElementById("count").innerText = value;
+//     }
+
+//     function decrement() {
+//       value--;
+//       document.getElementById("count").innerText = value;
+//     }
+
+
+function getTotal(price,qty){
+    return price*qty;
 }
-greet()
 
+function showbill(){
+  let q = Number(document.getElementById("price").value)
+  let p = Number(document.getElementById("quality").value)
+  let total = getTotal(q,p)
+  document.getElementById("bill").innerText = `<h1>Total bill : ${total} </h1>`;
 
+}
 
-// increment and decrement
-
-let value = 0;
-
-    function increment() {
-      value++;
-      document.getElementById("count").innerText = value;
-    }
-
-    function decrement() {
-      value--;
-      document.getElementById("count").innerText = value;
-    }
