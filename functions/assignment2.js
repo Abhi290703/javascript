07e6872cc7cf2c4f930692a5c2c3b85f52c909b8
Abhi290703 <abhigec6@gmail.com>
2025-09-07 15:15:36 +0530

    add assign2

---
+++ functions/assignment2.js
@@ -0,0 +1,10 @@
+let cartmessage = ""
+
+function order(){
+    alert(cartmessage + " you order an item");
+}
+
+function addcart(){
+    // cartmessage("you added an item")
+    alert(cartmessage + "you added an item to cart");
+}
