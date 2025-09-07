629240f8618c7a0c84cd827c54d4905a4e864777
Abhi290703 <abhigec6@gmail.com>
2025-09-07 21:23:18 +0530

    add all

--- functions/assignment2.js
+++ functions/assignment2.js
@@ -1,11 +1,35 @@
-let cartmessage = ""
+// program 1
 
-function order(){
-    alert(cartmessage + " you order an item");
-}
+// let cartmessage = ""
 
-function addcart() {
-    alert(cartmessage + "you added an item to cart");
-}
+// function order(){
+//     alert(cartmessage + " you order an item");
+// }
+
+// function addcart() {
+//     alert(cartmessage + "you added an item to cart");
+// }
+
+// function createstars (count){
+//     let star = "";
+//     for (let i = 0; i<count; i++){
+//         star += "&#9734";
+//     }
+//     document.getElementById("star").innerHTML = star
+// }
+// createstars(5);
+
+// or
+
+let stars = document.querySelectorAll(".star"); 
+
+stars.forEach(function(star) {
+  star.addEventListener("click", function() {
+    stars.forEach(s => s.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
+
+
 
 
