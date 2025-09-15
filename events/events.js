1d8ff72e06beda8facd86ba598350d0da3667659
Abhi290703 <abhigec6@gmail.com>
2025-09-15 15:02:03 +0530

    add events

---
+++ events/events.js
@@ -0,0 +1,118 @@
+/* 
+
+what is event?
+event is an action occurance recogenized by the browser that can be handled by java script.
+
+eg. to do some action by using particular thing is called event
+
+in events their are some types
+
+1.inline event : 
+        inline event is nothing but we will code inside the tag is called inline event
+    Advantages :
+    1.it is simply to write and easy to understand
+    2.in simple projects it is sutiable
+
+2.event property :
+        their are some event properties where we use to perform some action on it
+    Advantages :
+    1.js is sperated from html
+    2.cleaner code than inline
+
+    dis - advantage :
+    1.can attach only ine handler  - > where it takes last occurance value rather than previous ones
+
+3. add event listner :
+        where we use add event in it.
+    Advantages :
+    1. can attach multiple handlers
+    2. js is sperated for html
+    3. cleaner code than inline
+
+
+*/
+
+
+
+
+// programs 
+
+function sayhello() {
+    alert("hello! welcome ")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
