664fc9124f51eabe05a25ef1fa07aff3059e1c23
Abhi290703 <abhigec6@gmail.com>
2025-10-01 21:46:43 +0530

    add forms

--- forms/forms.js
+++ forms/forms.js
@@ -2,3 +2,8 @@ document.getElementById("my form").addEventListener("submit",(e) => {
 e.preventDefault();
 console.log("form submitted");
 })
+
+document.getElementById("youtube").addEventListener("click",(e) => {
+    e.preventDefault()
+    console.log("click youtube")
+})
