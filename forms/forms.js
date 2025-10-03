a3366e19789f93059c584c1efbd506d20e32d332
Abhi290703 <abhigec6@gmail.com>
2025-10-03 18:29:15 +0530

    add forms

--- forms/forms.js
+++ forms/forms.js
@@ -24,25 +24,3 @@
 //     e.target.style.background = "white"
 // })
 
-
-
-document.getElementById("my form").addEventListener("submit",(e) => {
-e.preventDefault();
-console.log("form submit");
-})
-
-document.getElementById("color").addEventListener("change", (e) => {
-  alert("Color changed: " + e.target.value);
-})
-
-document.getElementById("name").addEventListener("click",(e) => {
-    console.log("current input :", + e.target.value)
-})
-
-document.getElementById("name").addEventListener("focus", (e) => {
-    e.target.style.background = "lightblue"
-})
-
-document.getElementById("name").addEventListener("blur", (e) => {
-    e.target.style.background = "white"
-})
