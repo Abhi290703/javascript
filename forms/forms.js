e6bd7bfa67771423388b96eee31f64f707e15428
Abhi290703 <abhigec6@gmail.com>
2025-10-03 16:40:37 +0530

    add forms

--- forms/forms.js
+++ forms/forms.js
@@ -7,3 +7,17 @@ document.getElementById("youtube").addEventListener("click",(e) => {
     e.preventDefault()
     console.log("click youtube")
 })
+
+document.getElementById("color").addEventListener("change",(e) => {
+    alert("color changed :" + e.target.value);
+})
+
+document.getElementById("name").addEventListener("input",(e) => {
+    console.log("current input :", + e.target.value)
+})
+
+document.getElementById("name").addEventListener("focus",(e) => {
+    e.target.style.background = "lightblue"
+})
+
+
