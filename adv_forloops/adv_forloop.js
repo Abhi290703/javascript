dcbc125abf16d3dc48f5dae6859f471e22929069
Abhi290703 <abhigec6@gmail.com>
2025-10-08 10:47:08 +0530

    adv_forloop.js

---
+++ adv_forloops/adv_forloop.js
@@ -0,0 +1,19 @@
+/*
+
+Map :
+it creates a new array by applying a function to each and every element of the original element
+does not change the original one it modifies.
+
+Reduce :
+it reduce an array to single value
+
+filter :
+extracts only satisfy elements it should be either (true/false)
+
+*/
+
+// program
+
+
+
+
