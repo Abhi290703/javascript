3431613651746196ba09a6a723fd5e3a162687b0
Abhi290703 <abhigec6@gmail.com>
2025-10-08 10:47:58 +0530

    add adv_forloop.js

--- adv_forloops/adv_forloop.js
+++ adv_forloops/adv_forloop.js
@@ -4,6 +4,10 @@ Map :
 it creates a new array by applying a function to each and every element of the original element
 does not change the original one it modifies.
 
+syntax :
+("array.map(function(element, index, array) { ... })")
+
+
 Reduce :
 it reduce an array to single value
 
